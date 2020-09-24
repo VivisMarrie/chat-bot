240d35094724d7c1e045497cdea3320ae396f277
Viviane <viv.sousa@hotmail.com>
2020-09-24 20:23:31 -0300

    updates

--- react/Chatbot.jsx
+++ react/Chatbot.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect } from "react";
 
-declare global {
-  interface Window {
-    kommunicate: any;
-  }
-}
-interface ChatbotProps {}
-const Chatbot: StorefrontFunctionComponent<ChatbotProps> = ({}) => {
+const Chatbot = () => {
   useEffect(() => {
     obtenhaConfig();
   }, []);
 
   function obtenhaConfig() {
     (function(_, m) {
+      var cssChanges =
+        ".km-carousel-card-title-wrapper {   display: flex;  justify-content: center;} .km-carousel-card-header-without-img{height: 0.15em} .km-carousel-card-header-with-img {height:25em} .km-carousel-card-img{height: 100%} ";
+
       var kommunicateSettings = {
         appId: "d118248f66cc6c4efa1c9523ce51e4fc",
         popupWidget: true,
         automaticChatOpenOnNavigation: true,
+        onInit: function() {
+          Kommunicate.customizeWidgetCss(cssChanges);
+        },
       };
       var s = document.createElement("script");
       s.type = "text/javascript";
